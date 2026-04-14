50b85f56febc2db82fef45822c745d7ee25c6b20
KlimVS <mow_centers0@icloud.com>
2026-04-14 09:56:17 +0300

    Initial Commit

---
+++ 03-1_ RockPaperScissors/_3_1__RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_1__RockPaperScissorsApp.swift
+//  03-1_ RockPaperScissors
+//
+//  Created by Vladislav on 14.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct _3_1__RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
