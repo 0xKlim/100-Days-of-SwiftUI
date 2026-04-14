4df47c35b8c1fd9d1fda4b8d33a618d071db342a
KlimVS <mow_centers0@icloud.com>
2026-04-14 15:23:45 +0300

    Initial Commit

---
+++ 04_BetterRest/_4_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_BetterRestApp.swift
+//  04_BetterRest
+//
+//  Created by Vladislav on 14.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct _4_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
