4e2e6e3c26ccc900006ab4eba4e4bd56b1ce3d6d
KlimVS <mow_centers0@icloud.com>
2026-04-15 16:28:09 +0300

    Initial Commit

---
+++ 05_ WordScramble/_5__WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5__WordScrambleApp.swift
+//  05_ WordScramble
+//
+//  Created by Vladislav on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct _5__WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
