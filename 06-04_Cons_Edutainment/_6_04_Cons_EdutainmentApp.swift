44bdb4840a8f1d591f07eb10457cfec4a1c3be4f
KlimVS <mow_centers0@icloud.com>
2026-04-22 14:48:40 +0300

    Initial Commit

---
+++ 06-04_Cons_Edutainment/_6_04_Cons_EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_04_Cons_EdutainmentApp.swift
+//  06-04_Cons_Edutainment
+//
+//  Created by Vladislav on 22.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct _6_04_Cons_EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
