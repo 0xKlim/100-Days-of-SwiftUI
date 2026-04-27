6c60e56f6f5dbf72d2d598730a91616879b357cc
KlimVS <mow_centers0@icloud.com>
2026-04-27 11:01:48 +0300

    Initial Commit

---
+++ 07-1_iExpense/_7_1_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7_1_iExpenseApp.swift
+//  07-1_iExpense
+//
+//  Created by Vladislav on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct _7_1_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
