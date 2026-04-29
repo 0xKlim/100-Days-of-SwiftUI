f1abdf42b8afba8f88254c8822db74f97e2e6eb2
KlimVS <mow_centers0@icloud.com>
2026-04-29 15:27:48 +0300

    Initial Commit

---
+++ 09_Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  09_Navigation
+//
+//  Created by Vladislav on 29.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 09_Navigation/_9_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_NavigationApp.swift
+//  09_Navigation
+//
+//  Created by Vladislav on 29.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct _9_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
