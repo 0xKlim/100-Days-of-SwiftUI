40df5063032f49dec8b0efe01f6ea680b9cb5b66
KlimVS <mow_centers0@icloud.com>
2026-05-04 11:37:55 +0300

    Initial Commit

---
+++ 10_CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  10_CupcakeCorner
+//
+//  Created by Vladislav on 04.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 10_CupcakeCorner/_0_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_CupcakeCornerApp.swift
+//  10_CupcakeCorner
+//
+//  Created by Vladislav on 04.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct _0_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
