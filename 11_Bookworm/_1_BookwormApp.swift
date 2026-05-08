921379c50074b9046944e59795d767e8ba290611
KlimVS <mow_centers0@icloud.com>
2026-05-08 10:51:18 +0300

    Initial Commit

---
+++ 11_Bookworm/_1_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_BookwormApp.swift
+//  11_Bookworm
+//
+//  Created by Vladislav on 08.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct _1_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
