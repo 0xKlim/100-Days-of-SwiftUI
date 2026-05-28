1ec83a9f9cd0f2ebc882588fb5416064fef492f9
KlimVS <mow_centers0@icloud.com>
2026-05-28 14:43:39 +0300

    Initial Commit

---
+++ 13_Instafilter/_3_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_InstafilterApp.swift
+//  13_Instafilter
+//
+//  Created by Vladislav on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct _3_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
