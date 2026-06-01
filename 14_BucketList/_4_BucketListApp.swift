82afda35c030180c564f016087daecaa53474a1e
KlimVS <mow_centers0@icloud.com>
2026-06-01 16:59:18 +0300

    Initial Commit

---
+++ 14_BucketList/_4_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_BucketListApp.swift
+//  14_BucketList
+//
+//  Created by Vladislav on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct _4_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
