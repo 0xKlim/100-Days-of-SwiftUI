839742c0716e46efbf3f860f3cd477b89797e2f2
KlimVS <mow_centers0@icloud.com>
2026-07-01 13:29:20 +0300

    Initial Commit

---
+++ -06_Consolidation_Collegs/_06_Consolidation_CollegsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _06_Consolidation_CollegsApp.swift
+//  -06_Consolidation_Collegs
+//
+//  Created by Vladislav on 01.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct _06_Consolidation_CollegsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
