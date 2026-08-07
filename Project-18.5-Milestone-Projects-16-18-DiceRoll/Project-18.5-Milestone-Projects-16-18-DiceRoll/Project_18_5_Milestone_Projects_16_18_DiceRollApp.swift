167948379cfbe4021baa4e0381b2c7da10a8c71d
KlimVS <mow_centers0@icloud.com>
2026-08-07 12:25:24 +0300

    feat: initial setup for Project-18.5-Milestone-DiceRoll

---
+++ Project-18.5-Milestone-Projects-16-18-DiceRoll/Project-18.5-Milestone-Projects-16-18-DiceRoll/Project_18_5_Milestone_Projects_16_18_DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_18_5_Milestone_Projects_16_18_DiceRollApp.swift
+//  Project-18.5-Milestone-Projects-16-18-DiceRoll
+//
+//  Created by Vladislav on 07.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project_18_5_Milestone_Projects_16_18_DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
