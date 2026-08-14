d73369a971665e14f7ec0e39f973779ab96b5147
KlimVS <mow_centers0@icloud.com>
2026-08-14 12:06:42 +0300

    feat: initial setup for Project-19-SnowSeeker

---
+++ Project-19-SnowSeeker/Project-19-SnowSeeker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-19-SnowSeeker
+//
+//  Created by Vladislav on 14.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-19-SnowSeeker/Project-19-SnowSeeker/Project_19_SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_19_SnowSeekerApp.swift
+//  Project-19-SnowSeeker
+//
+//  Created by Vladislav on 14.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project_19_SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
