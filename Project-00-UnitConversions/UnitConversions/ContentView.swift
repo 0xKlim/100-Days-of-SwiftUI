a8596c393566cf1d118c9afb4f79f27f3fbf4d4b
KlimVS <mow_centers0@icloud.com>
2026-09-14 16:50:00 +0300

    feat: implement UnitConversions project

--- Project-00-UnitConversions/UnitConversions/ContentView.swift
+++ Project-00-UnitConversions/UnitConversions/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputValue = 0.0
+    @State private var inputType = UnitTemperature.celsius
+    @State private var outputType = UnitTemperature.kelvin
+    
+    private let units: [UnitTemperature] = [.celsius, .fahrenheit, .kelvin]
+    
+    private var baseMeasurement: Measurement<UnitTemperature> {
+        let inputMeasurement = Measurement(value: inputValue, unit: inputType)
+        return inputMeasurement.converted(to: .kelvin)
+    }
+    
+    private var outputMeasurement: Measurement<UnitTemperature> {
+        baseMeasurement.converted(to: outputType)
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section ("Input type") {
+                    Picker("Input type", selection: $inputType) {
+                        ForEach(units, id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Input value") {
+                    TextField("Value", value: $inputValue, format: .number)
+                }
+                
+                Section ("Output  type") {
+                    Picker("Output type", selection: $outputType) {
+                        ForEach(units, id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Output value") {
+                    Text("\(outputMeasurement.formatted(.measurement(width: .abbreviated, usage: .asProvided)))")
+                }
+            }
+            .navigationTitle("Unit Conversions")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
