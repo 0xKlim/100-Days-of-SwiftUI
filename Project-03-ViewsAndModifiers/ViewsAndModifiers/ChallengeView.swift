b094d35da33fc11be004e41d35d8503406f813aa
KlimVS <mow_centers0@icloud.com>
2026-09-16 11:23:51 +0300

    feat(Project-03): [challenge] create prominent blue title ViewModifier

--- Project-03-ViewsAndModifiers/ViewsAndModifiers/ChallengeView.swift
+++ Project-03-ViewsAndModifiers/ViewsAndModifiers/ChallengeView.swift
@@ -10,9 +10,24 @@ import SwiftUI
 struct ChallengeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .prominentTitle()
     }
 }
 
 #Preview {
     ChallengeView()
+}
+
+struct ProminentTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.blue)
+    }
+}
+
+extension View {
+    func prominentTitle() -> some View {
+        modifier(ProminentTitle())
+    }
 }
