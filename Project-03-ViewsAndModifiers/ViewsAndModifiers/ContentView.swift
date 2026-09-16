3a1dd2531b2fbb9d4571f5baf4e2ec295756c894
KlimVS <mow_centers0@icloud.com>
2026-09-16 11:11:00 +0300

    feat(Project-03): implement ViewsAndModifiers project

--- Project-03-ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project-03-ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,16 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("r\(row) c\(col)")
         }
-        .padding()
+        .background(.red.opacity(0.5))
+        .watermarked(with: "watermark")
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black.opacity(0.5))
+                .clipShape(.capsule)
+                .padding(5)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
 }
